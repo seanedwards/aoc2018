45154609ebd550d85ab649c747122d95fc455605
Sean Edwards <stedwards87@gmail.com>
2018-12-01 10:32:04 -0500

    Idioms for the idiom gods

--- lib/day01/day01.ex
+++ lib/day01/day01.ex
@@ -1,19 +1,19 @@
 defmodule Day01 do
   use Aoc2018, day: 1
 
-  def inputAsNumbers do
-    String.split(input(), "\n")
+  def input_as_numbers do
+    String.split(input(), "\r\n")
     |> Enum.filter(&(&1 != ""))
     |> Enum.map(&String.to_integer/1)
   end
 
-  def partOne do
-    inputAsNumbers()
+  def part_one do
+    input_as_numbers()
     |> Enum.sum
   end
 
-  def partTwo do
-    inputAsNumbers()
+  def part_two do
+    input_as_numbers()
     |> Stream.cycle
     |> Stream.scan([], &Day01.accumulator/2)
     |> Stream.drop_while(fn([ freq | seen ]) -> !Enum.member?(seen, freq) end)
